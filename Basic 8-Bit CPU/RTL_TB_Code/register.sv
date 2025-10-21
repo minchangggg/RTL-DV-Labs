09ab6d6f2970edd2691f505084c7fffc5c441a69
meynchan <125820144+minchangggg@users.noreply.github.com>
2025-10-21 18:43:55 +0700

    Update register.sv

--- Basic 8-Bit CPU/RTL_TB_Code/register.sv
+++ Basic 8-Bit CPU/RTL_TB_Code/register.sv
@@ -1,19 +1,25 @@
+/* register.sv — 8-bit load-enable register
+ * Behavior:
+   - rst_ is asynchronous and active low
+   - The register is clocked on the rising edge of clk.
+   - If enable is high, the input data is passed to the output out.
+   - Otherwise, the current value of out is retained in the register.
+ */
+
 module register (
-	input	wire clk,
-	input   wire enable,
-	input   wire rst_,
-	input 	wire [7:0] data,
-	output	reg  [7:0] out
+  input  wire       clk,
+  input  wire       rst_,
+  input  wire       enable,
+  input  wire [7:0] data, 
+  output reg  [7:0] out
 );
+  
+  timeunit 1ns; 
+  timeprecision 100ps;
 
-timeunit 1ns;
-timeprecision 100ps;
-
-always_ff @(posedge clk, negedge rst_)
-	if (!rst_)
-		out <= '0;
-	else if (enable)
-		out <= data;
-	else
-		out <= out;
+  always_ff @(posedge clk or negedge rst_) begin
+    if (!rst_)       out <= '0; 
+    else if (enable) out <= data; 
+    else             out <= out;  
+  end
 endmodule
