2f93d677925e1d12b376c4d90b4469de4deddf51
meynchan <125820144+minchangggg@users.noreply.github.com>
2025-10-21 20:03:41 +0700

    Update scale_mux.sv

--- Basic 8-Bit CPU/RTL_TB_Code/scale_mux.sv
+++ Basic 8-Bit CPU/RTL_TB_Code/scale_mux.sv
@@ -1,19 +1,28 @@
-module scale_mux #(WIDTH = 1)
+/* scale_mux.sv - Simple scalable MUX 
+ * Simple scalable MUX using an always_comb + unique case construct
+ * Behavior:
+   - in_a, in_b and out are all logic vectors
+   - The MUX width is parameterized with a default value of 1
+   - If sel_a is 1'b1, input in_a is passed to the output
+   - If sel_a is 1'b0, input in_b is passed to the output
+ */
+
+module scale_mux #(parameter int WIDTH = 1) 
 (
-	output logic [WIDTH-1:0] out,
-	input  logic [WIDTH-1:0] in_a,
-	input  logic [WIDTH-1:0] in_b,
-	input  logic sel_a
+  input  logic [WIDTH-1:0] in_a, 
+  input  logic [WIDTH-1:0] in_b,
+  input  logic             sel_a,
+  output logic [WIDTH-1:0] out
 );
+  
+  timeunit 1ns;
+  timeprecision 100ps;
 
-timeunit 1ns;
-timeprecision 100ps;
-
-always_comb
-	unique case (sel_a)
-		1'b1: out = in_a;
-		1'b0: out = in_b;
-		default: out = 'x;
-	endcase
-
-endmodule
+  always_comb 
+    unique case (sel_a)
+      1'b1: out = in_a;
+      1'b0: out = in_b;
+      default: out = 'x; // defensive coding
+    endcase
+  
+endmodule : scale_mux
